05ac45bb1277c03f17e438454ed65d4abadc6128
Martina Juri <martina.juri@mi.unc.edu.ar>
2025-06-16 14:31:37 -0300

    update

---
+++ asm/rutinas.asm
@@ -0,0 +1,222 @@
+;--- Rutinas para el proyecto ---
+
+; --- Rutina para multiplexar los displays ---
+MOSTRAR_DISPLAY:
+    CALL    DISPLAY_CENTENAS
+    CALL    DISPLAY_DECENAS
+    CALL    DISPLAY_UNIDADES   
+    RETURN
+DISPLAY_CENTENAS:
+    MOVF    centenas, W
+    CALL    TABLA_7SEG
+    MOVWF   PORTD
+
+    ; Activar RA5 (centenas)
+    MOVLW   b'00000110'
+    MOVWF   PORTE
+    CALL    RETARDO            
+    MOVLW   b'00000111'
+    MOVWF   PORTE          
+    RETURN
+    
+DISPLAY_DECENAS:
+    MOVF    decenas, W
+    CALL    TABLA_7SEG
+    MOVWF   PORTD
+    BSF	    PORTD, 7            ; Punto decimal
+
+    ; Activar RA6 (decenas)
+    MOVLW   b'00000101'
+    MOVWF   PORTE
+    CALL    RETARDO            
+    MOVLW   b'00000111'
+    MOVWF   PORTE          
+    RETURN
+
+DISPLAY_UNIDADES:
+    MOVF    unidades, W
+    CALL    TABLA_7SEG
+    MOVWF   PORTD
+
+    ; Activar RA7 (unidades)
+    MOVLW   b'00000011'
+    MOVWF   PORTE
+    CALL    RETARDO            
+    MOVLW   b'00000111'
+    MOVWF   PORTE          
+    RETURN
+
+; --- Rutina de retardo visible para multiplexado (16ms) ---
+RETARDO:
+    BANK0
+    CLRF TMR0               ; Limpiar TMR0
+    BCF INTCON,TMR0IF       ; Limpia el flag de interrupción TMR0
+LOOP_RETARDO:
+    BTFSS INTCON, TMR0IF    ; Espera a que TMR0 se desborde
+    GOTO LOOP_RETARDO       ; Si no se desbordó, espera
+    RETURN
+
+; --- Rutina de ADC ---
+ADC:
+    BANK0
+    BCF     PORTC, 4        ; Apagar LED RC1 (fin del ciclo)
+
+    ; Prueba de conversión ADC
+    MOVLW   b'00000000'
+    MOVWF   ADRESH
+    BANK1
+    MOVLW   b'00000000'
+    MOVWF   ADRESL
+    
+    ; Actualiza centenas, decenas y unidades
+    CALL    SEPARAR_3DIGITOS
+
+    ; Iniciar nueva conversión ADC
+    BSF     ADCON0, GO
+    BSF     PORTC, 4        ; Prender LED RC1 (inicio ADC)
+    RETURN
+
+
+; --- Tabla de conversión a 7 segmentos (anodo común) ---
+TABLA_7SEG
+    ADDWF   PCL, F
+    RETLW   0xC0    ; 0
+    RETLW   0xF9    ; 1
+    RETLW   0xA4    ; 2
+    RETLW   0xB0    ; 3
+    RETLW   0x99    ; 4
+    RETLW   0x92    ; 5
+    RETLW   0x82    ; 6
+    RETLW   0xF8    ; 7
+    RETLW   0x80    ; 8
+    RETLW   0x90    ; 9
+
+; -----------------------------------
+; en algun init llamen a INIT_UART
+; y que en las variables definen las necesarias acá
+; decenas, unidades y decimas
+; spdecenas, spunidades y spdecimas
+
+
+
+INIT_UART:
+    BSF     STATUS, RP0
+    MOVLW   0x19          ; 9600 baud @ 4MHz -> SPBRG = 25
+    MOVWF   SPBRG
+    BCF     STATUS, RP0
+
+    BSF     TXSTA, BRGH   ; High speed
+    BSF     TXSTA, TXEN   ; Enable TX
+    BSF     RCSTA, SPEN   ; Enable serial port (TX/ RX)
+    RETURN
+
+; =============================
+; === TX mensaje ===
+; =============================
+
+TX_TEXTO:
+    ; Envía: Temp: xx.x°C, Set Point: xx.x°C\r\n
+    ; Envía caracteres con CALL TX_BYTE
+    ; Armarlo como:
+    ; "Temp: ", número, ", Set Point: ", número, "\r\n"
+    MOVLW   'T'  ; ejemplo
+    CALL    TX_BYTE
+    MOVLW   'e'
+    CALL    TX_BYTE
+    MOVLW   'm'
+    CALL    TX_BYTE
+    MOVLW   'p'
+    CALL    TX_BYTE
+    MOVLW   ':'
+    CALL    TX_BYTE
+    MOVLW   ' '
+    CALL    TX_BYTE
+
+    ; Parte entera
+    ; si tienen algo como decenas, unidades y decimas
+    MOVF    decenas, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVF    unidades, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVLW   '.'
+    CALL    TX_BYTE
+
+    MOVF    decimas, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVLW   223           ; ° (grados)
+    CALL    TX_BYTE
+    MOVLW   'C'
+    CALL    TX_BYTE
+    MOVLW   ','
+    CALL    TX_BYTE
+    MOVLW   ' '
+    CALL    TX_BYTE
+
+    ; "Set Point: "
+    MOVLW   'S'
+    CALL    TX_BYTE
+    MOVLW   'e'
+    CALL    TX_BYTE
+    MOVLW   't'
+    CALL    TX_BYTE
+    MOVLW   ' '
+    CALL    TX_BYTE
+    MOVLW   'P'
+    CALL    TX_BYTE
+    MOVLW   'o'
+    CALL    TX_BYTE
+    MOVLW   'i'
+    CALL    TX_BYTE
+    MOVLW   'n'
+    CALL    TX_BYTE
+    MOVLW   't'
+    CALL    TX_BYTE
+    MOVLW   ':'
+    CALL    TX_BYTE
+    MOVLW   ' '
+    CALL    TX_BYTE
+
+    ; Set point
+    ; si tienen algo como spdecenas, spunidades y spdecimas
+    MOVF    spdecenas, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVF    spunidades, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVLW   '.'
+    CALL    TX_BYTE
+
+    MOVF    spdecimas, W
+    CALL    TX_ASCII       ; Enviar 1 caracter
+
+    MOVLW   223
+    CALL    TX_BYTE
+    MOVLW   'C'
+    CALL    TX_BYTE
+    MOVLW   0x0D ; '\r'
+    CALL    TX_BYTE
+    MOVLW   0x0A ; '\n'
+    CALL    TX_BYTE
+    RETURN
+
+; =============================
+; === TX byte por UART ===
+; =============================
+TX_BYTE:
+    BTFSS   PIR1, TXIF  ; espera si está aún transmitiendo
+    GOTO    $-1          
+    MOVWF   TXREG
+    RETURN
+
+; =============================
+; === Enviar 1 dígito (ascii ===
+; 0 --> "0", 1 --> "1" ... 9-->"9"
+; =============================
+TX_ASCII:
+    ADDLW   '0'
+    CALL    TX_BYTE
+    RETURN
